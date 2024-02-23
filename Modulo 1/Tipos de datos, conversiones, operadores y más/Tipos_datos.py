adc07c3875eee07769fd8736dd7c9eb28b81ecdd
NancyM87 <nancym.sisa@gmail.com>
2024-02-23 11:40:19 -0500

    my second commit

---
+++ Modulo 1/Tipos de datos, conversiones, operadores y más/Tipos_datos.py
@@ -0,0 +1,4 @@
+#Strings(str)->Se utiliza para almacenar cadenas de caracteres.Ejemplos: Letras, símbolos,textos,números.
+#Los Strings van dentro de comillas simples('') o dobles("").
+print('"Red" es rojo en español')
+print("'Red' es rojo en español")
