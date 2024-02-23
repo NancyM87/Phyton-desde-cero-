ac022197e195e8ac12ed5420d82e3c9189cc09fb
NancyM87 <nancym.sisa@gmail.com>
2024-02-23 15:38:36 -0500

    four commit

--- Modulo 1/Tipos de datos, conversiones, operadores y más/Tipos_datos.py
+++ Modulo 1/Tipos de datos, conversiones, operadores y más/Tipos_datos.py
@@ -4,6 +4,9 @@ print('"Red" es rojo en español')
 print("'Red' es rojo en español")
 #Función len()->Devuelve el número de caracteres que tiene un string.
 print(len("Nancyta"))
+#len en un número entero
+numeros="12345678"
+print(len(numeros))
 #En una variable
 nombre="Nancyta"
 print(len(nombre))
@@ -39,3 +42,59 @@ modulo=numero1%numero2
 print(modulo)
 ##Exponente(**)
 print(5**3)
+#Float->Es un tipo de dato que sirve para representar valores con números decimales.
+num_decimal=20.45
+print(num_decimal)  
+#Boolean->Es un tipo de dato que puede recibir 2 valores True o False.
+#Como ver el tipo de dato en Phyton
+num1=5
+num2=89.78
+texto="Hoa que tal como te va"
+boolean=False
+print(type(num1))
+print(type(num2))
+print(type(texto))
+print(type(boolean))
+#Transformar un integer a un string
+num_entero=4567
+num_str=str(num_entero)
+print(len(num_str))
+#Transformar string en integer
+num1="20"
+num2="40"
+suma=int(num1) + int(num2)
+print(suma)
+#Truncamiento de números
+n1=10.60
+n2=10.40
+suma=n1+n2
+trunca_suma=int(suma)
+print(trunca_suma)
+#Redondeo de números
+numeros=7.5678*6.943534
+print(numeros)
+print(round(numeros))
+#Redondeo de números con 2 deciamles
+num=7.5678*6.943534
+print(round(num,2))
+#Operador de floor division->Realiza divisiones y redondea el resultado al valor entero mas cercano.(//)
+division=10//3
+print(division)
+#Operador módulo(%)->Devuelve el resto de la divisio entera
+modulo=10%3
+print(modulo)
+#Operadores de incremento y decremento
+#Operador (+=)
+num=10
+num+=30
+print(num)
+#Operador (-=)
+num=6
+num-=9
+print(num)
+#Formateo de strings
+suma=20+12
+print('La suma es:' + str(suma))
+#Utilizando prefijo f
+suma=90+45
+print(f'La suma es:{suma}')
