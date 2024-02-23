6ca9b62be272f3315df4291184dc069e75e858a4
NancyM87 <nancym.sisa@gmail.com>
2024-02-23 12:11:57 -0500

    three commit

--- Modulo 1/Tipos de datos, conversiones, operadores y más/Tipos_datos.py
+++ Modulo 1/Tipos de datos, conversiones, operadores y más/Tipos_datos.py
@@ -2,3 +2,40 @@
 #Los Strings van dentro de comillas simples('') o dobles("").
 print('"Red" es rojo en español')
 print("'Red' es rojo en español")
+#Función len()->Devuelve el número de caracteres que tiene un string.
+print(len("Nancyta"))
+#En una variable
+nombre="Nancyta"
+print(len(nombre))
+#Posiciones de un string, la posición de la letra a es 0.
+color="azul"
+print(color[0])
+#Integers-> Es un tipo de dato que representa valores numéricos enteros.
+#Operaciones con enteros
+#Suma
+numero1=100
+numero2=20
+suma=numero1+numero2
+print(suma)
+#Resta
+numero1=20
+numero2=-100
+resta=numero1-numero2
+print(resta)
+#Multiplicación
+numero1=5
+numero2=6
+multiplicacion=numero1*numero2
+print(multiplicacion)
+#Division
+numero1=20
+numero2=5
+division=numero1/numero2
+print(division)
+#Módulo
+numero1=20
+numero2=2
+modulo=numero1%numero2
+print(modulo)
+##Exponente(**)
+print(5**3)
